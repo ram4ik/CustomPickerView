206e62de29b3b80dcff55b42b7fd298045a4b6a0
Ramill Ibragimov <Ramill_Ibragimov@icloud.com>
2020-03-02 22:23:16 +0200

    init

--- CustomPickerView/ContentView.swift
+++ CustomPickerView/ContentView.swift
@@ -9,8 +9,23 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var selected = "Monday"
+    
     var body: some View {
-        Text("Hello, World!")
+        ZStack {
+            Color.orange.edgesIgnoringSafeArea(.all)
+            VStack {
+                Spacer()
+                Text(self.selected)
+                    .padding()
+                    .foregroundColor(.white)
+                    .font(.largeTitle)
+                Spacer()
+                CustomPicker(selected: self.$selected)
+                Spacer()
+            }
+        }
     }
 }
 
@@ -19,3 +34,86 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+struct CustomPicker: UIViewRepresentable {
+    
+    @Binding var selected: String
+    
+    func makeCoordinator() -> CustomPicker.Coordinator {
+        return CustomPicker.Coordinator(parent1: self)
+    }
+
+    func makeUIView(context: UIViewRepresentableContext<CustomPicker>) -> UIPickerView {
+        let picker = UIPickerView()
+        picker.dataSource = context.coordinator
+        picker.delegate = context.coordinator
+        return picker
+    }
+    
+    func updateUIView(_ uiView: UIPickerView, context: UIViewRepresentableContext<CustomPicker>) {
+        
+    }
+    
+    class Coordinator: NSObject, UIPickerViewDelegate, UIPickerViewDataSource {
+        var parent: CustomPicker
+        
+        init(parent1: CustomPicker) {
+            parent = parent1
+        }
+        
+        func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+            return data.count
+        }
+        
+        func numberOfComponents(in pickerView: UIPickerView) -> Int {
+            return 1
+        }
+        
+//        func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+//            return data[row]
+//        }
+        
+        func pickerView(_ pickerView: UIPickerView, viewForRow row: Int, forComponent component: Int, reusing view: UIView?) -> UIView {
+            let view = UIView(frame: CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width - 100, height: 60))
+            
+            let label = UILabel(frame: CGRect(x: 0, y: 0, width: view.bounds.width, height: view.bounds.height))
+            
+            label.text = data[row]
+            label.textColor = .white
+            label.textAlignment = .center
+            label.font = .systemFont(ofSize: 22, weight: .bold)
+            
+            view.backgroundColor = .clear
+            
+            view.addSubview(label)
+            
+            view.clipsToBounds = true
+            view.layer.cornerRadius = view.bounds.height / 2
+            
+            view.layer.borderWidth = 2.5
+            view.layer.borderColor = UIColor.white.cgColor
+            
+            return view
+        }
+        
+        func pickerView(_ pickerView: UIPickerView, widthForComponent component: Int) -> CGFloat {
+            return UIScreen.main.bounds.width - 100
+        }
+        
+        func pickerView(_ pickerView: UIPickerView, rowHeightForComponent component: Int) -> CGFloat {
+            return 67
+        }
+        
+        func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+            self.parent.selected = data[row]
+        }
+    }
+}
+
+class Host: UIHostingController<ContentView> {
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
+}
+
+var data = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
